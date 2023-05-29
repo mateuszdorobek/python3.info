b2ca16cbb305a23b622f0f67c1ef096809631358
Matt Harasymczuk <matt@astrotech.io>
2023-05-29 12:12:55 +0200

    Advanced: OOP #time 20m

---
+++ advanced/oop/assignments/oop_attribute_mutability_d.py
@@ -0,0 +1,52 @@
+"""
+* Assignment: OOP AttributeMutability Dataclass Randint
+* Complexity: easy
+* Lines of code: 3 lines
+* Time: 3 min
+
+English:
+    1. Create dataclass `User`, with attributes:
+        a. `name: str` (required)
+        b. `health: int` (optional), default: randint(50, 100)
+        c. `gold: int` (optional), default: randint(1, 100)
+    2. Attributes must be set st the initialization from constructor arguments
+    3. Avoid mutable parameter problem
+    4. Run doctests - all must succeed
+
+Polish:
+    1. Stwórz dataklasę `User`, z atrybutami:
+        a. `name: str` (wymagane)
+        b. `health: int` (opcjonalne), domyślnie: randint(50, 100)
+        c. `gold: int` (opcjonalne), domyślnie: randint(1, 100)
+    2. Atrybuty mają być ustawiane przy inicjalizacji z parametrów konstruktora
+    3. Uniknij problemu motowalnych parametrów
+    4. Uruchom doctesty - wszystkie muszą się powieść
+
+Tests:
+    >>> import sys; sys.tracebacklimit = 0
+    >>> from inspect import isclass
+
+    >>> assert isclass(User)
+    >>> assert hasattr(User, '__annotations__')
+
+    >>> assert 'name' in User.__dataclass_fields__
+    >>> assert 'health' in User.__dataclass_fields__
+    >>> assert 'gold' in User.__dataclass_fields__
+"""
+from dataclasses import dataclass, field
+from random import randint
+
+
+# Modify class `User` to avoid mutable parameter problem
+# type: type[User]
+@dataclass
+class Hero:
+    ...
+
+
+# Solution
+@dataclass
+class User:
+    name: str
+    health: int = field(default_factory=lambda: randint(50, 100))
+    gold: int = field(default_factory=lambda: randint(1, 100))
